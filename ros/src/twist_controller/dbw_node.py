10fde016c1d1a5b06259fd7cd6d669bb4e98c4f2
juxingzhutou <juxingzhutou@gmail.com>
2018-12-07 03:13:22 +0800

    Fix DBW node issue

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -58,14 +58,16 @@ class DBWNode(object):
                  wheel_base, steer_ratio, max_lat_accel, max_steer_angle)
 
         # TODO: Subscribe to all the topics you need to
+        rospy.Subscriber('/vehicle/dbw_enabled', Bool, self.dbw_enabled_cb)
+        rospy.Subscriber('/twist_cmd', TwistStamped, self.twist_cb)
+        rospy.Subscriber('/current_velocity', TwistStamped, self.velocity_cb)
+
+
         self.current_vel = None
         self.curr_ang_vel = None
-
         self.dbw_enabled = None
-
         self.linear_vel = None
         self.angular_vel = None
-
         self.throttle = self.steering = self.brake = 0
 
         self.loop()
@@ -91,6 +93,16 @@ class DBWNode(object):
             if self.dbw_enabled:
                 self.publish(self.throttle, self.brake, self.steering)
             rate.sleep()
+
+    def dbw_enabled_cb(self, msg):
+        self.dbw_enabled = msg
+
+    def twist_cb(self, msg):
+        self.linear_vel = msg.twist.linear.x
+        self.angular_vel = msg.twist.angular.z
+
+    def velocity_cb(self, msg):
+        self.current_vel = msg
 
     def publish(self, throttle, brake, steer):
         tcmd = ThrottleCmd()
