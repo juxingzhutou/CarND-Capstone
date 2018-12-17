a216f904665d9c9718e1e48ec3ff88ae91c135df
juxingzhutou <juxingzhutou@gmail.com>
2018-12-17 13:37:38 +0800

    Fix error log

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -24,6 +24,10 @@ class TLDetector(object):
         self.camera_image = None
         self.lights = []
 
+        self.base_waypoints = None
+        self.waypoints_2d = None
+        self.waypoint_tree = None
+
         sub1 = rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         sub2 = rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
 
@@ -50,10 +54,6 @@ class TLDetector(object):
         self.last_state = TrafficLight.UNKNOWN
         self.last_wp = -1
         self.state_count = 0
-
-        self.base_waypoints = None
-        self.waypoints_2d = None
-        self.waypoint_tree = None
 
         self.is_in_sim = True
 
